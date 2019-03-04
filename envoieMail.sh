02e4dddc6fe8f5ffec9d3cd6b647d4c7c3083de1
blackantrax <37629170+blackantrax@users.noreply.github.com>
2019-03-03 19:04:12 -0500

    Update envoieMail.sh

--- envoieMail.sh
+++ envoieMail.sh
@@ -2,7 +2,7 @@
 
 #Permet d'envoyer le username et le mot de passe des utilisateurs
 
-# Recoit comme parametre le username et le mot de passe
+# Recoit comme parametre le username, le nom,le E-mail et le mot de passe de l'utilisateur concerne.
 
 function envoie(){
 	echo -e "Bonjour $4\nVoici vos infos utilisateur.\nUsername: $1\nPassword: $2\n\nCEO\nKevin Nelson" | mutt -s "Creation compte" $3 
